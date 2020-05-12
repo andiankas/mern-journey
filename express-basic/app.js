776f49cc3e7248fadfe2d75cad00cdda9cc19dba
Andi Rustianto <rustiantoandi17@gmail.com>
2020-05-12 13:53:47 +0700

    install template engine ejs

---
+++ express-basic/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'ejs') // register the template engine
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.get('/:name',(req,res) => res.send(`Nama saya : ${req.params.name}`))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
