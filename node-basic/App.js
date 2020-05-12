f33cfdd7aa08a2903e1180d863569f0ef111f327
Andi Rustianto <rustiantoandi17@gmail.com>
2020-05-12 12:58:46 +0700

    install module npm superheroes

--- node-basic/App.js
+++ node-basic/App.js
@@ -1,3 +1,9 @@
 // const fs = require("fs");
 // fs.copyFileSync("text.txt","text2.txt");
 // console.log("Copy file success")
+
+const hero = require("superheroes");
+
+for (let i = 0; i < 10; i++) {
+  console.log(i+". "+hero.random());
+}
